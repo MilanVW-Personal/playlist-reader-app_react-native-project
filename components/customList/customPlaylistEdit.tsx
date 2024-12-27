62c339fde38a84434182cbd9db59c72e66fe2c61
MilanVW1 <r0985432@student.thomasmore.be>
2024-12-27 17:51:20 +0100

    styling van editpagina aangepast, komt overeen met stijl create pagina + overzicht songs

--- components/customList/customPlaylistEdit.tsx
+++ components/customList/customPlaylistEdit.tsx
@@ -27,36 +27,69 @@ const CustomPlaylistEdit: FunctionComponent<CustomPlaylistDetailProps> = ({id ,t
   return (
     <View>
       <View>
-        <Label>Edit title: </Label>
-        <TextInput value={newTitle} onChangeText={setNewTitle}/>
-      </View>
-      <View>
-        <Label>Edit playlist description: </Label>
-        <TextInput value={newDesc} onChangeText={setNewDesc}/>
+        <Text style={styles.screenTitle}>Edit playlist</Text>
+        <View style={styles.fields}>
+          <Text style={styles.inputLabel}>Playlist title: </Text>
+          <TextInput
+            style={styles.inputField}
+            value={newTitle}
+            onChangeText={setNewTitle}
+          />
+        </View>
+        <View style={styles.fields}>
+          <Text style={styles.inputLabel}>Playlist description: </Text>
+          <TextInput
+            style={styles.inputField}
+            value={newDesc}
+            onChangeText={setNewDesc}
+          />
+        </View>
+        <View style={styles.fields}> {/*Hier komt swipe to delete gesture*/}
+          <Text style={styles.inputLabel}>Songs:  </Text>
+          {songs?.map((s, index) => {
+            return <Text style={styles.songCardItem} key={index}>{s.title}</Text>
+          })}
+        </View>
+        <View style={styles.fields}>
+          <Button title={'Submit changes'} onPress={submitChanges} />
+        </View>
       </View>
 
-      <View style={styles.card}>
-        {songs?.map((s, index) => {
-          return <Text key={index}>{s.title}</Text>
-        })}
-      </View>
-      <Button title={"Submit changes"} onPress={submitChanges}/>
+
     </View>
   )
 }
 const styles = StyleSheet.create({
-  card: {
-    borderWidth: 2,
-    borderColor: 'lightgray',
-    borderRadius: 10,
-    textAlign: 'left',
-    marginTop: '50%',
-    marginLeft: 10,
-    marginRight: 10,
-    height: 'auto',
-    width: 'auto',
+  screenTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  fields: {
+    margin: 10,
+  },
+  inputLabel: {
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  inputField: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderRadius: 2,
+    paddingLeft: 10,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  songCardItem: {
+    marginTop: 5,
     padding: 5,
+    borderColor: 'gray',
+    fontSize: 15,
+    borderWidth: 0.5,
+    width: 'auto',
+    height: 'auto'
   },
 })
-
 export default CustomPlaylistEdit
